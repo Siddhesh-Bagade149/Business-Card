fa6ca96ec9fe7a33f7f5d293087219ffb29f0693
Siddhesh Bagade <siddheshbagade1618@gmail.com>
2024-02-03 10:12:37 +0530

    working rn with inputs but ugly css

--- src/App.jsx
+++ src/App.jsx
@@ -2,56 +2,57 @@ import { useState } from "react";
 import "./App.css";
 import { BusinessCard } from "../component/BusinessCard";
 
-let inputData = {};
-function handleSubmit(e) {
-  e.preventDefault();
-
-  // Read the form data
-  const form = e.target;
-  const formData = new FormData(form);
-
-  // You can pass formData as a fetch body directly:
-  // fetch('/some-api', { method: form.method, body: formData });
-
-  // Or you can work with it as a plain object:
-  // console.log(formData.entries())
-  const formJson = Object.fromEntries(formData.entries());
-
-  // console.log(formJson);
-  inputData = formJson;
-}
-console.log(inputData);
-
 function App() {
+  const [name, setName] = useState("");
+  const [descp, setDescp] = useState("");
+  const [number, setNumber] = useState("");
+  const [email, setEmail] = useState("");
+  const [linkedin, setLinkedin] = useState("");
   return (
     <div>
-      <form method="post" onSubmit={handleSubmit}>
-        <input type="text" name="name" placeholder="enter name"></input>
+
+      <form>
+        <input
+          type="text"
+          name="name"
+          placeholder="enter name"
+          onChange={(e) => setName(e.target.value)}
+        ></input>
         <input
           type="text"
           name="descp"
           placeholder="enter short description"
+          onChange={(e) => setDescp(e.target.value)}
         ></input>
         <input
           type="text"
           name="phone"
           placeholder="enter phone number"
+          onChange={(e) => setNumber(e.target.value)}
         ></input>
-        <input type="text" name="email" placeholder="enter email"></input>
+        <input
+          type="text"
+          name="email"
+          placeholder="enter email"
+          onChange={(e) => setEmail(e.target.value)}
+        ></input>
         <input
           type="text"
           name="linkedin"
           placeholder="enter linkedin url"
+          onChange={(e) => setLinkedin(e.target.value)}
         ></input>
-        <button type="submit">Submit</button>
+        {/* <button type="submit">Submit</button> */}
       </form>
+
       <BusinessCard
-        name={inputData.name}
-        description={inputData.descp}
-        phone={inputData.number}
-        linkedIn={inputData.linkedin} //pass as https or it will not open
-        email={inputData.email}
+        name={name}
+        description={descp}
+        phone={number}
+        linkedIn={linkedin} //pass as https or it will not open
+        email={email}
       />
+      
     </div>
   );
 }
